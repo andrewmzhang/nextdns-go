6f3f195d530e1d43b9b0352613ce6da14131ed46
Andrew M. Zhang <andy@andywebservices.com>
2026-02-10 15:07:31 -0600

    Add rewrites (#5)
    * Add rewrites
    
    * Reduce README.md

---
+++ services/rewrites.go
@@ -0,0 +1,53 @@
+package services
+
+import (
+	"github.com/andrewmzhang/nextdns-go/models"
+)
+
+type BoundRewrite struct {
+	boundPath     string
+	nextDNSClient *NextDNSClient
+	// GetableResource[models.Rewrite]
+	// UpdatableResource[models.Rewrite]
+	DeletableResource[models.Rewrite]
+}
+
+func (b *BoundRewrite) SetBind(nextDNSClient *NextDNSClient, bindPath string) *BoundRewrite {
+	if b == nil {
+		b = &BoundRewrite{}
+	}
+	// b.GetableResource.boundPath = bindPath
+	// b.GetableResource.nextDNSClient = nextDNSClient
+	// b.UpdatableResource.boundPath = bindPath
+	// b.UpdatableResource.nextDNSClient = nextDNSClient
+	b.DeletableResource.boundPath = bindPath
+	b.DeletableResource.nextDNSClient = nextDNSClient
+	return b
+}
+
+type RewriteService struct {
+	// path string
+	ListableResource[models.Rewrite]
+	CreatableResource[models.Rewrite]
+	BindableResource[models.Rewrite, *BoundRewrite]
+}
+
+func (c *NextDNSClient) Rewrites(profileId string) *RewriteService {
+	r := &RewriteService{
+		ListableResource: ListableResource[models.Rewrite]{
+			nextDNSClient: c,
+			path:          "/profiles/" + profileId + "/rewrites",
+		},
+		CreatableResource: CreatableResource[models.Rewrite]{
+			nextDNSClient: c,
+			path:          "/profiles/" + profileId + "/rewrites",
+		},
+		BindableResource: BindableResource[models.Rewrite, *BoundRewrite]{
+			nextDNSClient: c,
+			bindGeneratingFn: func(s string) string {
+				return "/profiles/" + profileId + "/rewrites/" + s
+			},
+		},
+	}
+	return r
+}
