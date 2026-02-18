76cb150911a9799a9d08b860143f8a144d5c9722
Andrew M. Zhang <andy@andywebservices.com>
2026-02-17 18:05:55 -0600

    Re-implement privacy w/ interfaces

--- services/privacys.go
+++ services/privacys.go
@@ -5,35 +5,42 @@ import (
 )
 
 type BoundPrivacy struct {
-	boundPath     string
-	nextDNSClient *NextDNSClient
 	err           error
+	pathFmt       string
+	pathArgs      map[string]interface{}
+	nextDNSClient *NextDNSClient
 	GetableResource[models.Privacy, *BoundPrivacy]
-	UpdatableResource[models.Privacy]
-	// DeletableResource[models.Privacy]
+	UpdatableResource[models.Privacy, *BoundPrivacy]
+}
+
+func (b *BoundPrivacy) InitBoundResource(nextDNSClient *NextDNSClient, pathFmt string, pathArgs map[string]interface{}, err error) *BoundPrivacy {
+	if b == nil {
+		b = &BoundPrivacy{}
+	}
+	b.nextDNSClient = nextDNSClient
+	b.pathFmt = pathFmt
+	b.pathArgs = pathArgs
+	b.err = err
+	b.GetableResource.parent = b
+	b.UpdatableResource.parent = b
+	return b
+}
+
+func (b *BoundPrivacy) GetNextDNSClient() *NextDNSClient {
+	return b.nextDNSClient
+}
+
+func (b *BoundPrivacy) GetBoundPath() (string, error) {
+	var path string
+	path, b.err = renderPath(b.pathFmt, b.pathArgs)
+	return path, b.err
 }
 
 func (b *BoundPrivacy) GetError() error {
 	return b.err
 }
 
-func (b *BoundPrivacy) SetBind(nextDNSClient *NextDNSClient, pathFmt string, pathArgs map[string]interface{}) T {
-	if b == nil {
-		b = &BoundPrivacy{}
-	}
-	b.GetableResource.parent = b
-	b.GetableResource.nextDNSClient = nextDNSClient
-	b.UpdatableResource.boundPath = bindPath
-	b.UpdatableResource.nextDNSClient = nextDNSClient
-	// b.DeletableResource.boundPath = bindPath
-	// b.DeletableResource.nextDNSClient = nextDNSClient
-	return b
-}
-
 type PrivacyService struct {
-	// pathFmt string
-	// ListableResource[models.Privacy]
-	// CreatableResource[models.Privacy]
 	BindableResource[models.Privacy, *BoundPrivacy]
 }
 
@@ -41,9 +48,8 @@ func (c *NextDNSClient) Privacy(profileId string) *BoundPrivacy {
 	r := &PrivacyService{
 		BindableResource: BindableResource[models.Privacy, *BoundPrivacy]{
 			nextDNSClient: c,
-			bindGeneratingFn: func(s string) string {
-				return "/profiles/" + s + "/privacy"
-			},
+			pathFmt:       "/profiles/{{ .profileId }}/privacy",
+			pathArgs:      map[string]interface{}{"profileId": profileId},
 		},
 	}
 	return r.Bind(profileId)
